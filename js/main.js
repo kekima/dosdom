62348b96935fb05c9e863cc9d00d1f421249c33a
senaya <vivisectivecarrot@gmail.com>
2020-02-27 01:22:37 +0200

    modal window added
    didn't even break anything

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById("modal-box");
+let btn = document.getElementById("modal-btn");
+let span = document.getElementsByClassName("modal-close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
