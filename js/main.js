ef87a7e484b863172f2ce71972282bfd88570a34
senaya <vivisectivecarrot@gmail.com>
2020-02-27 02:08:38 +0200

    modal windows fix
    now multiple elements can address same modal window: cycle goes through the collection of html elements with the target class and adds an event listener to all of them.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@ let modal = document.getElementById("modal-box");
 let btn = document.getElementById("modal-btn");
 let span = document.getElementsByClassName("modal-close")[0];
 
-btn.onclick = function() {
+
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+
+let modalButton = document.getElementsByClassName("modal-button-trigger");
+
+for (i = 0; i < modalButton.length; i++) {
+  modalButton[i].onclick = function () {
+    modal.style.display = "block";
   }
 }
