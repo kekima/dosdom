825dbbaeeb97d453f56f6000617ba392cadc8a15
senaya <vivisectivecarrot@gmail.com>
2020-07-27 17:23:59 +0200

    form fixing

--- sendform.php
+++ sendform.php
@@ -2,14 +2,15 @@
 
 if(isset($_POST['submit'])) {
     $to = "vivisectivecarrot@gmail.com";
-    $from = "marcovkir@gmail.com";
+    $from = "";
     $name = $_POST['clientName'];
     $phone = $_POST['clientPhone'];
     $subject = "Заказ рассчёта проекта";
     $message = $name . " " . " заинтересовван в рассчёте проекта, его телефон: " . $phone;
-    $header = "From:" . $from;
+    $headers[] = 'From: Заказ рассчёта проекта <marcovkir@gmail.com>';
+    //$header = "From:" . $from;
 
-    mail($to,$subject,$message,$header);
+    mail($to,$subject,$message, implode("\r\n", $headers));
     echo "Почта отправлена.";
 }
 
